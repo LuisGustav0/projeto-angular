84167f4eb47da51877263c23b6cfb73940003b12
luisgustavo <luis.gustav0@live.com>
2018-11-10 14:09:08 -0200

    primeiro commit

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { BemVindoComponent } from './bem-vindo/bem-vindo.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    BemVindoComponent,
+    JumbotronComponent
   ],
   imports: [
     BrowserModule
