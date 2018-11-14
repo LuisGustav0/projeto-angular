8d8735a1fa47fb718c0524db14b636176ac66fa6
Luis Gustavo <luisgustavo@prodata.inf.br>
2018-11-14 13:11:20 -0200

    primeiro commit

--- demo-innovation-ui/projects/innovation-ui/src/lib/module/components/input-text/input-text.component.ts
+++ demo-innovation-ui/projects/innovation-ui/src/lib/module/components/input-text/input-text.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'in-input-text',
   templateUrl: './input-text.component.html',
   styleUrls: ['./input-text.component.css']
 })
-export class InputTextComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class InputTextComponent {
+  @Input() label: string;
+  @Input() type: string;
+  @Input() placeholder: string;
 }
