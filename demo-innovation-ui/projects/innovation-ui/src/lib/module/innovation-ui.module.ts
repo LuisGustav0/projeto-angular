7bd873601a2b38268d4258cb17edde6c39e6d935
Luis Gustavo <luisgustavo@prodata.inf.br>
2018-11-23 14:13:03 -0200

    primeiro commit

--- demo-innovation-ui/projects/innovation-ui/src/lib/module/innovation-ui.module.ts
+++ demo-innovation-ui/projects/innovation-ui/src/lib/module/innovation-ui.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 
+import {
+  MatToolbarModule,
+  MatSidenavModule,
+  MatButtonModule  
+} from '@angular/material';
+
 import { IndexComponent } from './components/index/index.component';
 import { InputTextComponent } from './components/input-text/input-text.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     IndexComponent,
-    InputTextComponent
+    InputTextComponent,
+    ButtonComponent
   ],
   imports: [
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule
   ],
   exports: [
     IndexComponent,
-    InputTextComponent
+    InputTextComponent,
+    ButtonComponent
   ]
 })
 export class InnovationUiModule { }
--- demo-innovation-ui/projects/innovation-ui/src/public_api.ts
+++ demo-innovation-ui/projects/innovation-ui/src/public_api.ts
@@ -4,3 +4,4 @@
 
 export * from './lib/module/innovation-ui.module';
 export * from './lib/module/components/input-text/input-text.component';
+export * from './lib/module/components/button/button.component';
