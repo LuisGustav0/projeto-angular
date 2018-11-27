23ba7ab3808a26684f630542b27c8258e02ab8a5
luisgustavo <luis.gustav0@live.com>
2018-11-26 23:50:17 -0200

    primeiro commit

--- demo-innovation-ui/projects/innovation-ui/src/lib/module/components/index/menu-list-item/menu-list-item.component.ts
+++ demo-innovation-ui/projects/innovation-ui/src/lib/module/components/index/menu-list-item/menu-list-item.component.ts
@@ -13,34 +13,15 @@ import {
 export class MenuListItemComponent implements OnInit {
   expanded: boolean;
   @HostBinding('attr.aria-expanded') ariaExpanded = this.expanded;
-  @Input() item: NavItem;
+  @Input() item: any;
   @Input() depth: number;
 
-  constructor(public navService: NavService,
-    public router: Router) {
-    if (this.depth === undefined) {
-      this.depth = 0;
-    }
+  constructor() {
   }
 
   ngOnInit() {
-    this.navService.currentUrl.subscribe((url: string) => {
-      if (this.item.route && url) {
-        // console.log(`Checking '/${this.item.route}' against '${url}'`);
-        this.expanded = url.indexOf(`/${this.item.route}`) === 0;
-        this.ariaExpanded = this.expanded;
-        // console.log(`${this.item.route} is expanded: ${this.expanded}`);
-      }
-    });
   }
 
-  onItemSelected(item: NavItem) {
-    if (!item.children || !item.children.length) {
-      this.router.navigate([item.route]);
-      this.navService.closeNav();
-    }
-    if (item.children && item.children.length) {
-      this.expanded = !this.expanded;
-    }
+  onItemSelected(item: any) {
   }
 }
