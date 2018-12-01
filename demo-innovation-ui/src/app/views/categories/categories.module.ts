467946e9a5c8aa01ccbcd83d4b7148b52b60a53d
luisgustavo <luis.gustav0@live.com>
2018-12-01 13:37:16 -0200

    primeiro commit

--- demo-innovation-ui/src/app/views/categories/categories.module.ts
+++ demo-innovation-ui/src/app/views/categories/categories.module.ts
@@ -2,18 +2,21 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
+import { InnovationUiModule } from './../../../../projects/innovation-ui/src/lib/module/innovation-ui.module';
+
 import { CadastroCategoriesComponent } from './cadastro-categories/cadastro-categories.component';
 import { PesquisaCategoriesComponent } from './pesquisa-categories/pesquisa-categories.component';
 
 @NgModule({
+  imports: [
+    CommonModule,
+    CategoriesRoutingModule,
+    InnovationUiModule
+  ],
   declarations: [
     CadastroCategoriesComponent,
     PesquisaCategoriesComponent
   ],
-  imports: [
-    CommonModule,
-    CategoriesRoutingModule
-  ],
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class CategoriesModule { }
