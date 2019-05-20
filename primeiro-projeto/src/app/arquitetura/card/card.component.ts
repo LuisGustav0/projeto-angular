265fc4858e36caaa473f636e193c15a91d22122c
luisgustavo <luis.gustav0@live.com>
2019-05-20 11:23:13 -0300

    primeiro commit

--- primeiro-projeto/src/app/arquitetura/card/card.component.ts
+++ primeiro-projeto/src/app/arquitetura/card/card.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  Input 
+} from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,11 +11,12 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  @Input() colaborador: any;
+  @Input() card: any;
+  @Input() propriedadeDescricao: string;
+  @Input() propriedadeImage: string;
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
